6974ccbabbfdf24a32ca8390aa39dbc4a0f58929
Eunseo Kim <eunddo2001@gmail.com>
2024-08-26 03:53:42 +0900

    change format

--- src/DEX.sol
+++ src/DEX.sol
@@ -94,11 +94,11 @@ contract Dex {
     }
 
     function sqrt(uint x) internal returns (uint y) {
-    uint z = (x + 1) / 2;
-    y = x;
-    while (z < y) {
-        y = z;
-        z = (x / z + z) / 2;
+        uint z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
     }
-}
 }
